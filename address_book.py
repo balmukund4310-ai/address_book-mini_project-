688b31854e0fbc235aaf9d70c898b7363a05b5f5
Balmukund Jha <balmukund@cusb.ac.in>
2021-05-31 10:21:30 +0530

    Update address_book.py

--- address_book.py
+++ address_book.py
@@ -3,17 +3,17 @@ from tkinter import *
 
 #initialize window
 root = Tk()
-root.geometry('400x400')
-root.config(bg = 'SlateGray3')
-root.title('DataFlair-AddressBook')
+root.geometry('600x400')
+root.config(bg = '#042b7a')
+root.title('My-AddressBook')
 root.resizable(0,0)
 contactlist = [
-    ['Parv Maheswari',  '0176738493'],
-    ['David Sharma',  '2684430000'],
-    ['Mandish Kabra',   '4338354432'],
-    ['Prisha Modi','6834552341'],
-    ['Rahul kaushik',   '1234852689'],
-    ['Johena Shaa' , '2119876543'],
+    ['abhishek',  '9878765767'],
+    ['ankit',  '6767676766'],
+    ['Mohan kumar  ',   '7635844632'],
+    ['Narendra Modi','9897675645'],
+    ['mohit khanna',   '09988776543'],
+    ['papu' , '8987565403'],
     ]
 
 Name = StringVar()
@@ -25,13 +25,13 @@ frame = Frame(root)
 frame.pack(side = RIGHT)
 
 scroll = Scrollbar(frame, orient=VERTICAL)
-select = Listbox(frame, yscrollcommand=scroll.set, height=12)
+select = Listbox(frame, yscrollcommand=scroll.set, height=18)
 scroll.config (command=select.yview)
 scroll.pack(side=RIGHT, fill=Y)
 select.pack(side=LEFT,  fill=BOTH, expand=1)
 
 
-########### function to get select value
+##*******************************function to get select value*********************************
 
 def Selected():
     return int(select.curselection()[0])
@@ -46,28 +46,22 @@ def EDIT():
     contactlist[Selected()] = [Name.get(), Number.get()]
     Select_set()
     
+    
+   # to view selected contact(first select then click on view button)
+def VIEW():
+    NAME, PHONE = contactlist[Selected()]
+    Name.set(NAME)
+    Number.set(PHONE)
+ 
 #to delete selected contact
 def DELETE():
     del contactlist[Selected()]
     Select_set()
    
-# to view selected contact(first select then click on view button)
-def VIEW():
-    NAME, PHONE = contactlist[Selected()]
-    Name.set(NAME)
-    Number.set(PHONE)
-
-
 ###exit game window   
 def EXIT():
     root.destroy()
-
-#empty name and number field
-def RESET():
-    Name.set('')
-    Number.set('')
-
-
+    
 def Select_set() :
     contactlist.sort()
     select.delete(0,END)
@@ -76,6 +70,11 @@ def Select_set() :
 Select_set()
 
 
+#empty name and number field
+def RESET():
+    Name.set('')
+    Number.set('')
+
 
 ######define buttons #####labels and entry widget
 Label(root, text = 'NAME', font='arial 12 bold', bg = 'SlateGray3').place(x= 30, y=20)
@@ -83,10 +82,10 @@ Entry(root, textvariable = Name).place(x= 100, y=20)
 Label(root, text = 'PHONE NO.', font='arial 12 bold',bg = 'SlateGray3').place(x= 30, y=70)
 Entry(root, textvariable = Number).place(x= 130, y=70)
 
-Button(root,text=" ADD", font='arial 12 bold',bg='SlateGray4', command = AddContact).place(x= 50, y=110)
-Button(root,text="EDIT", font='arial 12 bold',bg='SlateGray4',command = EDIT).place(x= 50, y=260)
-Button(root,text="DELETE", font='arial 12 bold',bg='SlateGray4',command = DELETE).place(x= 50, y=210)
-Button(root,text="VIEW", font='arial 12 bold',bg='SlateGray4', command = VIEW).place(x= 50, y=160)
+Button(root,text=" ADD", font='arial 12 bold',bg='#84ADF9', command = AddContact).place(x= 50, y=110)
+Button(root,text="EDIT", font='arial 12 bold',bg='#84ADF9',command = EDIT).place(x= 50, y=260)
+Button(root,text="DELETE", font='arial 12 bold',bg='#84ADF9',command = DELETE).place(x= 50, y=210)
+Button(root,text="VIEW", font='arial 12 bold',bg='#84ADF9', command = VIEW).place(x= 50, y=160)
 Button(root,text="EXIT", font='arial 12 bold',bg='tomato', command = EXIT).place(x= 300, y=320)
 Button(root,text="RESET", font='arial 12 bold',bg='SlateGray4', command = RESET).place(x= 50, y=310)
 
